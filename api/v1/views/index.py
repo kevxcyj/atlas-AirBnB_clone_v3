0eabbe2c1d5d5074884cc27f7243955010f62bff
kevxcyj <kevcyjunior@gmail.com>
2024-07-15 11:49:24 -0500

    Rearanged import statements

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """ Retuns status for app_views """
 
+from api.v1.views import app_views
 from flask import jsonify
 from models import storage
-from api.v1.views import app_views
 
 @app_views.route('/status', strict_slashes=False)
 def status():

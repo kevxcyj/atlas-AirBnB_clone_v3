57b89e6c75473bb730bf4f1dfaa5aca2e9b13a47
kevxcyj <kevcyjunior@gmail.com>
2024-07-15 11:44:30 -0500

    Addded new import method to add count into index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,7 @@
 from flask import jsonify
 from models import storage
 from api.v1.views import app_views
+from models import count
 
 @app_views.route('/status', strict_slashes=False)
 def status():

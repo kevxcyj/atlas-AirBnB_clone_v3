18ac8e24c1118748414eb1581963868506d80a6b
kevxcyj <kevcyjunior@gmail.com>
2024-07-15 07:57:17 -0500

    Added route to index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+""" Retuns status for app_views """
+
+from flask import jsonify
+from models import storage
+from . import app_views
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status():
+    return jsonify({"status": "OK"})

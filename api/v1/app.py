650a69f2b01307a51797adeae5c750b503175f4b
kevxcyj <kevcyjunior@gmail.com>
2024-07-15 10:51:54 -0500

    Added error handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """ This is used to connect to the API """
 
 
-from flask import Flask, Blueprint
+from flask import Flask, Blueprint, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
@@ -15,8 +15,12 @@ app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix="/api/v1")
 
 @app.teardown_appcontext
-def close_storage(exception):
+def close_storage(code):
     storage.close()
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return jsonify({"error": "Not found"}), 404
 
 if __name__ == "__main__":
     app.run(host=host, port=port, threaded=True)

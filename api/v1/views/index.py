928fcdb981a1835a91ec034886eff542ef153b3d
kevxcyj <kevcyjunior@gmail.com>
2024-07-15 08:15:52 -0500

    Added end point to index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,3 +8,20 @@ from . import app_views
 @app_views.route('/status', strict_slashes=False)
 def status():
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stat_count():
+    """ Endpoint that retrieves number of each objects """
+    count_stats = {
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User')
+    }
+    return jsonify(count_stats)
+
+
+if __name__ == "__main__":
+    pass

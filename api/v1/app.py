fa80def1739b6878a95437f57850630aa57d2123
kevxcyj <kevcyjunior@gmail.com>
2024-07-16 14:49:49 -0500

    Fixed pep8 in api

--- api/v1/app.py
+++ api/v1/app.py
@@ -19,9 +19,11 @@ cors = CORS(app, resources={'/*': {'origins': '0.0.0.0'}})
 def close_storage(code):
     storage.close()
 
+
 @app.errorhandler(404)
 def page_not_found(error):
     return jsonify({"error": "Not found"}), 404
+
 
 if __name__ == "__main__":
     app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,9 +5,11 @@ from api.v1.views import app_views
 from flask import jsonify
 from models import storage
 
+
 @app_views.route('/status', strict_slashes=False)
 def stat():
     return jsonify({"status": "OK"})
+
 
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def stat_count():
